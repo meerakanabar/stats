2697838b841815fa01963354f8086583e9c4d774
meerakanabar <mvk2110@caa.columbia.edu>
2015-10-16 16:51:08 -0400

    update2

--- prob_lending_club.py
+++ prob_lending_club.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as stats
 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
@@ -12,13 +13,10 @@ plt.savefig("boxplotInvestorsFunding.png")
 loansData.hist(column='Amount.Funded.By.Investors')
 plt.savefig("HistogramInvestorsFunding.png")
 
-import scipy.stats as stats
 
-plt.figure()
+plt.figure(1)
 graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
 plt.savefig("ProbabilityPlotInvestorsFunding.png")
-
-loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
 loansData.dropna(inplace=True)
 
@@ -30,8 +28,6 @@ plt.savefig("boxplotAmountRequested.png")
 loansData.hist('Amount.Requested')
 plt.savefig("AmountRequested.png")
 
-import scipy.stats as stats
-
-plt.figure()
+plt.figure(2)
 graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
 plt.savefig("AmountRequested.png")
