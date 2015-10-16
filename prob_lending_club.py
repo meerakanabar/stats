5fa887eaf65e53e078495c3c3ba56476eec4e7a5
meerakanabar <mvk2110@caa.columbia.edu>
2015-10-16 16:53:31 -0400

    update3

--- prob_lending_club.py
+++ prob_lending_club.py
@@ -7,27 +7,27 @@ loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loan
 loansData.dropna(inplace=True)
 
 loansData.boxplot(column='Amount.Funded.By.Investors')
-
+plt.figure(1)
 plt.savefig("boxplotInvestorsFunding.png")
 
 loansData.hist(column='Amount.Funded.By.Investors')
+plt.figure(2)
 plt.savefig("HistogramInvestorsFunding.png")
 
 
-plt.figure(1)
+plt.figure(3)
 graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
 plt.savefig("ProbabilityPlotInvestorsFunding.png")
 
-loansData.dropna(inplace=True)
-
 
 loansData.boxplot(column='Amount.Requested')
-
+plt.figure(4)
 plt.savefig("boxplotAmountRequested.png")
 
+plt.figure(5)
 loansData.hist('Amount.Requested')
 plt.savefig("AmountRequested.png")
 
-plt.figure(2)
+plt.figure(6)
 graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
 plt.savefig("AmountRequested.png")
